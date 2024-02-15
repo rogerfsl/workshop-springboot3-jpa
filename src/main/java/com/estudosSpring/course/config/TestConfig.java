dc2d79ccb7d19fb683b3b056b7f348ed3b94431a
rogerfsl <rogerfsl@hotmail.com>
2024-02-15 12:56:18 -0300

    OrderItem, many-to-many association with extra attributes Part 2

--- src/main/java/com/estudosSpring/course/config/TestConfig.java
+++ src/main/java/com/estudosSpring/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.estudosSpring.course.entities.Category;
 import com.estudosSpring.course.entities.Order;
+import com.estudosSpring.course.entities.OrderItem;
 import com.estudosSpring.course.entities.Product;
 import com.estudosSpring.course.entities.User;
 import com.estudosSpring.course.entities.enums.OrderStatus;
 import com.estudosSpring.course.repositories.CategoryRepository;
+import com.estudosSpring.course.repositories.OrderItemRepository;
 import com.estudosSpring.course.repositories.OrderRepository;
 import com.estudosSpring.course.repositories.ProductRepository;
 import com.estudosSpring.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -73,6 +78,13 @@ public class TestConfig implements CommandLineRunner{
 		Order o3 = new Order(null, Instant.parse("2024-01-30T18:05:00Z"),OrderStatus.WAITING_PAYMENT ,u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 		
 		
--- src/main/java/com/estudosSpring/course/entities/Order.java
+++ src/main/java/com/estudosSpring/course/entities/Order.java
@@ -2,10 +2,13 @@ package com.estudosSpring.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.estudosSpring.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -39,6 +43,9 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> itens = new HashSet<>();
+	
 	public Order() {
 		
 	}
@@ -85,6 +92,11 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public Set<OrderItem> getItens(){
+		return itens;
+	}
+	
 
 	@Override
 	public int hashCode() {
