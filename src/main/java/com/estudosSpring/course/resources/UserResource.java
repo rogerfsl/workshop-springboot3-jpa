bdb6252175a97e0dca6e08701426c7c706aa173e
rogerfsl <rogerfsl@hotmail.com>
2024-02-16 11:32:53 -0300

    User Update

--- src/main/java/com/estudosSpring/course/resources/UserResource.java
+++ src/main/java/com/estudosSpring/course/resources/UserResource.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudosSpring.course.entities.User;
 import com.estudosSpring.course.services.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -51,6 +53,13 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
