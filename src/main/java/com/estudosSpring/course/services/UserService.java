63232448c4af9800d87df5e65b9cd9bb21438ec6
rogerfsl <rogerfsl@hotmail.com>
2024-02-16 12:32:05 -0300

    Exception handling - findById

--- src/main/java/com/estudosSpring/course/services/UserService.java
+++ src/main/java/com/estudosSpring/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.estudosSpring.course.entities.User;
 import com.estudosSpring.course.repositories.UserRepository;
+import com.estudosSpring.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -23,7 +24,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/estudosSpring/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.estudosSpring.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		
+		super("Resource not found. id " + id);
+	}
+
+}
