2f816ac6ab0cabf452c64dedfb12c98fc900ed1c
rogerfsl <rogerfsl@hotmail.com>
2024-02-16 15:36:49 -0300

    Exception handling - update

--- src/main/java/com/estudosSpring/course/services/UserService.java
+++ src/main/java/com/estudosSpring/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.estudosSpring.course.repositories.UserRepository;
 import com.estudosSpring.course.resources.exceptions.DatabaseException;
 import com.estudosSpring.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -47,9 +49,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
